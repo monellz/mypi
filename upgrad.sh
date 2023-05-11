99e8a9162828fca7b0940d944d5bee5356e1a1a7
monellz <rxzhong@outlook.com>
2023-05-11 08:25:26 +0100

    init

---
+++ upgrad.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -x
+
+lsb_release -a
+sudo cp thu_apt_sources.list /etc/apt/sources.list
+sudo apt update
+sudo apt upgrade -y
+sudo apt -y install vim git python3-pip 
---
+++ vimrc
@@ -0,0 +1,5 @@
+set number
+set expandtab
+set ts=4
+set softtabstop=4
+set shiftwidth=4
