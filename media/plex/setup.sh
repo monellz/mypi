691b0e806c92ce1aa7d758b1fc8f5dd8c3a2eee2
monellz <rxzhong@outlook.com>
2023-05-17 19:35:33 +0800

    add plex

---
+++ media/plex/setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -x
+
+sudo apt update
+sudo apt-get install -y apt-transport-https
+curl https://downloads.plex.tv/plex-keys/PlexSign.key | gpg --dearmor | sudo tee /usr/share/keyrings/plex-archive-keyring.gpg > /dev/null
+echo deb [signed-by=/usr/share/keyrings/plex-archive-keyring.gpg] https://downloads.plex.tv/repo/deb public main | sudo tee /etc/apt/sources.list.d/plexmediaserver.list
+sudo apt-get update
+
+sudo apt install -y plexmediaserver
---
+++ misc/cpu_temp.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+for tzone in /sys/class/thermal/thermal_zone*; do
+  tzone_num=${tzone#*_zone}
+  if [[ $tzone == *"thermal_zone"* ]]; then
+    temp=$(cat "$tzone/temp")
+    temp=$((temp/1000))
+    echo "CPU$tzone_num temperature: $temp°C"
+  fi
+done
