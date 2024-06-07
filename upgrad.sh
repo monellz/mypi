623036da5562ee0a9d16fe1b93c5288c203cc75e
monellz <rxzhong@outlook.com>
2024-06-07 20:48:13 +0800

    fix bug in sending mail

--- upgrad.sh
+++ upgrad.sh
@@ -5,7 +5,7 @@ lsb_release -a
 sudo cp thu_apt_sources.list /etc/apt/sources.list
 sudo apt update
 sudo apt upgrade -y
-sudo apt install -y vim git python3-pip 
+sudo apt install -y vim git python3-pip tree
 
 # for dig
 sudo apt install -y dnsutils
